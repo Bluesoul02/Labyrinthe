048291b26aac80b74d512c6b4126a6bcb265ef58
Boisedu Maxence <maxence_boisedu@ens.univ-artois.fr>
2021-02-17 17:26:03 +0100

    pion, pionimpl et plateau

---
+++ src/PionImpl.java
@@ -0,0 +1,14 @@
+class PionImpl implements Pion{
+    private Plateau plateau;
+    private Position positionInitiale;
+    private Positon positionCourante;
+
+
+    PionImpl(){
+
+    }
+
+    public void deplacer(Position pos) {};
+
+    poserA(Position pos);
+}
