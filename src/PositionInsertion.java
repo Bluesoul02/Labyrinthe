c4475589b65d6dd5ba3fecf0c086808e1228603b
Grégoire Delacroix <gregoire_delacroix@ens.univ-artois.fr>
2021-03-01 20:17:30 +0100

    Update PositionInsertion.java

--- src/PositionInsertion.java
+++ src/PositionInsertion.java
@@ -1,3 +1,7 @@
 enum PositionInsertion {
-    N1, N2, N3, E1, E2, E3, S1, S2, S3, O1, O2, O3
+    N1, N2, N3, E1, E2, E3, S1, S2, S3, O1, O2, O3;
+
+    public PositionInsertion oppose() {
+        
+    }
 }
