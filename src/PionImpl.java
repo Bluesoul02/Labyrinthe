1e458472fbc1135efd7f6939874e96f60986b769
Arcadia <4826615-AvotK@users.noreply.gitlab.com>
2021-03-03 17:01:27 +0100

    espace

--- src/PionImpl.java
+++ src/PionImpl.java
@@ -1,20 +1,18 @@
-class PionImpl implements Pion{
+class PionImpl implements Pion {
     private Plateau plateau;
     private Position positionInitiale;
     private Position positionCourante;
 
-
-    PionImpl(){
+    PionImpl() {
 
     }
 
     public Objectif deplacer(Position pos) {
-        
+
     }
 
-
     void poserA(Position pos) {
-        if(plateau.estAtteignable(positionCourante, pos)) {
+        if (plateau.estAtteignable(positionCourante, pos)) {
             plateau.deplacer(pos, this);
             positionCourante = pos;
         }
