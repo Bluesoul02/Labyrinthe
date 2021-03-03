e52419f258eb6adc4851e5246160e05f748599fa
Arcadia <4826615-AvotK@users.noreply.gitlab.com>
2021-03-03 17:06:03 +0100

    correction

--- src/Jeu.java
+++ src/Jeu.java
@@ -1,7 +1,9 @@
+import java.util.List;
+
 interface Jeu {
   void modifierCouloirs(PositionInsertion pos, Orientation orientation);
 
   void enregistrer(Joueur joueur, Couleur couleur);
 
-  List<Couloirs> couloirs();
+  List<Couloir> couloirs();
 }
