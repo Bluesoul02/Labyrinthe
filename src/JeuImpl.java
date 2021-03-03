011d6070ad773ba9421736df405cfc205450cfe7
Arcadia <4826615-AvotK@users.noreply.gitlab.com>
2021-03-03 17:14:15 +0100

    correction

--- src/JeuImpl.java
+++ src/JeuImpl.java
@@ -1,64 +1,63 @@
 import java.util.*;
 
-class JeuImpl implements Jeu{
+class JeuImpl implements Jeu {
   private CouloirMobile supplementaire;
   private PositionInsertion positionOrigine;
   private List<Joueur> joueurs;
-  private Map<Couleur,Pion> pions;
+  private Map<Couleur, Pion> pions;
   private Objectif[] objectifs;
   private Plateau plateau;
 
-
-  JeuImpl(){
+  JeuImpl() {
     plateau = new Plateau();
     joueurs = new ArrayList<Joueur>();
     objectifs = new Objectif[24];
   }
 
-
-@Override
-public void modifierCouloirs(PositionInsertion pos, Orientation orientation) {
-	if(pos != positionOrigine) {
-    supplementaire = plateau.modifierCouloirs(pos, supplementaire);
-    positionOrigine = pos.oppose();
-    for(Pion pion : supplementaire.getPions()) {
-      pion.poserA(positionOrigine);
+  @Override
+  public void modifierCouloirs(PositionInsertion pos, Orientation orientation) {
+    if (pos != positionOrigine) {
+      supplementaire = plateau.modifierCouloirs(pos, supplementaire);
+      positionOrigine = pos.oppose();
+      for (Pion pion : supplementaire.getPions()) {
+        pion.poserA(positionOrigine);
+      }
     }
   }
-}
 
+  @Override
+  public void enregistrer(Joueur joueur, Couleur couleur) {
+    // TODO Auto-generated method stub
+    // donner un pion au joueur
 
-@Override
-public void enregistrer(Joueur joueur, Couleur couleur) {
-  // TODO Auto-generated method stub
-  // donner un pion au joueur
-	
-}
+  }
 
+  @Override
+  public List<Couloir> couloirs() {
+    // TODO Auto-generated method stub
+    // Doit créer les couloirs mobiles
+    return null;
+  }
 
-@Override
-public List<Couloirs> couloirs() {
-	// TODO Auto-generated method stub
-  // Doit créer les couloirs mobiles
-	return null;
-}
+  private void preparer() {
+    // TODO distribuer les objectifs aux joueurs, attribuer les pions aux joueurs,
+    // mettre les couloirs fixe par le biais du plateau
+  }
 
-private void preparer(){
-  // TODO distribuer les objectifs aux joueurs, attribuer les pions aux joueurs, mettre les couloirs fixe par le biais du plateau
-}
+  private void jouer() {
+    Joueur joueur;
+    do {
+      joueur = prochainJoueur();
+      joueur.joue();
+    } while (!aGagne(joueur));
+  }
 
-private void jouer(){
-  Joueur joueur;
-  do {
-    joueur = prochainJoueur();
-    joueur.joue();
-  } while(!aGagne(joueur));
-}
+  private boolean aGagne(Joueur joueur) {
+    return (joueur.objectifs.length() == 0 && joueur.pion.positionInitiale == joueur.pion.PositionCourante);
+  }
 
-private boolean aGagne(Joueur joueur) {
-  return (joueur.objectifs.length() == 0 && joueur.pion.positionInitiale == joueur.pion.PositionCourante);
-}
+  private Joueur prochainJoueur() {
 
-private Joueur prochainJoueur();
+  }
 
 }
--- src/Joueur.java
+++ src/Joueur.java
@@ -1,6 +1,9 @@
 interface Joueur {
     int getAge();
+
     void joue();
+
     void fixerObjectifs(Pile<Objectif> objectifs);
+
     void recevoirPion(Pion p);
 }
