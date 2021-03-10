77fc83207b12b41a50cb4b6c77c9175dcfb3be98
gregoire_delacroix <gregoire_delacroix@ens.univ-artois.fr>
2021-03-10 16:57:41 +0100

    set couloirs fix

--- src/Orientation.java
+++ src/Orientation.java
@@ -1,3 +1,3 @@
 enum Orientation {
-    NORD, SUD, EST, OUEST
+    NORD, EST, SUD, OUEST
 }
--- src/Plateau.java
+++ src/Plateau.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 class Plateau {
     private List<Couloir> couloirs;
+    private static final Random RAND = new Random();
 
     Plateau() {
         couloirs = new ArrayList<>();
@@ -21,6 +22,22 @@ class Plateau {
     }
 
     protected void setCouloirFixe() {
-        couloirs.push(new CouloirFixe(EST, forme, objectif, ));
+        Orientation[] orients = Orientation.values();
+        Couleur[] couleurs = Couleur.values();
+        for(int i=0; i<4; i++) {
+            new CouloirFixe(orients[i], Forme.COUDE, null, couleurs[i].getPositionInitiale());
+        }
+
+        Position[] positions = {new Position(2,2), new Position(2,4), new Position(4,2), new Position(4,4)};
+        Objectif[] objectifs = Objectif.values();
+        for(int i=0; i<4; i++) {
+            new CouloirFixe(orients[i], Forme.TE, objectifs[i], positions[i]);
+        }
+
+        int[][] values = {{0,2,0,4},{2,0,4,0},{6,2,6,4},{2,6,4,6}};
+        for(int i=0; i<4; i++) {
+            new CouloirFixe(orients[i], Forme.TE, objectifs[i], new Position(values[i][0],values[i][1]));
+            new CouloirFixe(orients[i], Forme.TE, objectifs[i], new Position(values[i][2],values[i][3]));
+        }
     }
 }
