517873cc93c864b98397fc9087a01f255e63657d
gregoire_delacroix <gregoire_delacroix@ens.univ-artois.fr>
2021-03-10 18:34:29 +0100

    Add couloirs fixes

--- src/Plateau.java
+++ src/Plateau.java
@@ -45,19 +45,19 @@ class Plateau {
         Orientation[] orients = Orientation.values();
         Couleur[] couleurs = Couleur.values();
         for (int i = 0; i < 4; i++) {
-            new CouloirFixe(orients[i], Forme.COUDE, null, couleurs[i].getPositionInitiale());
+            couloirs.add(new CouloirFixe(orients[i], Forme.COUDE, null, couleurs[i].getPositionInitiale()));
         }
 
         Position[] positions = { new Position(2, 2), new Position(2, 4), new Position(4, 2), new Position(4, 4) };
         Objectif[] objectifs = Objectif.values();
         for (int i = 0; i < 4; i++) {
-            new CouloirFixe(orients[i], Forme.TE, objectifs[i], positions[i]);
+            couloirs.add(new CouloirFixe(orients[i], Forme.TE, objectifs[i], positions[i]));
         }
 
         int[][] values = { { 0, 2, 0, 4 }, { 2, 0, 4, 0 }, { 6, 2, 6, 4 }, { 2, 6, 4, 6 } };
         for (int i = 0; i < 4; i++) {
-            new CouloirFixe(orients[i], Forme.TE, objectifs[i], new Position(values[i][0], values[i][1]));
-            new CouloirFixe(orients[i], Forme.TE, objectifs[i], new Position(values[i][2], values[i][3]));
+            couloirs.add(new CouloirFixe(orients[i], Forme.TE, objectifs[i], new Position(values[i][0], values[i][1])));
+            couloirs.add(new CouloirFixe(orients[i], Forme.TE, objectifs[i], new Position(values[i][2], values[i][3])));
         }
     }
 
@@ -67,7 +67,7 @@ class Plateau {
 
     protected Couloir getCouloir(Position pos) {
         for (int i = 0; i < couloirs.size(); i++) {
-            if (couloirs.get(i).getPosition() == pos)
+            if (couloirs.get(i).getPosition().x() == pos.x() && couloirs.get(i).getPosition().y() == pos.y())
                 if (couloirs.get(i).getClass() != CouloirMobile.class || ((CouloirMobile) couloirs.get(i)).isPosee())
                     return couloirs.get(i);
         }
