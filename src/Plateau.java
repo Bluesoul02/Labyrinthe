f4a0520f035638fb9e05a5fcd4fa6788ab9ea355
gregoire_delacroix <gregoire_delacroix@ens.univ-artois.fr>
2021-03-10 17:34:52 +0100

    m

--- src/Plateau.java
+++ src/Plateau.java
@@ -31,7 +31,6 @@ class Plateau {
 
         Position[] positions = { new Position(2, 2), new Position(2, 4), new Position(4, 2), new Position(4, 4) };
         Objectif[] objectifs = Objectif.values();
-<<<<<<< HEAD
         for(int i=0; i<4; i++) {
             new CouloirFixe(orients[i], Forme.TE, objectifs[RAND.nextInt(objectifs.length)], positions[i]);
         }
@@ -40,16 +39,6 @@ class Plateau {
         for(int i=0; i<4; i++) {
             new CouloirFixe(orients[i], Forme.TE, objectifs[RAND.nextInt(objectifs.length)], new Position(values[i][0],values[i][1]));
             new CouloirFixe(orients[i], Forme.TE, objectifs[RAND.nextInt(objectifs.length)], new Position(values[i][2],values[i][3]));
-=======
-        for (int i = 0; i < 4; i++) {
-            new CouloirFixe(orients[i], Forme.TE, objectifs[i], positions[i]);
-        }
-
-        int[][] values = { { 0, 2, 0, 4 }, { 2, 0, 4, 0 }, { 6, 2, 6, 4 }, { 2, 6, 4, 6 } };
-        for (int i = 0; i < 4; i++) {
-            new CouloirFixe(orients[i], Forme.TE, objectifs[i], new Position(values[i][0], values[i][1]));
-            new CouloirFixe(orients[i], Forme.TE, objectifs[i], new Position(values[i][2], values[i][3]));
->>>>>>> 4047108c3a7b8a67ef4bf32236ceacd9050a4c5a
         }
     }
 
