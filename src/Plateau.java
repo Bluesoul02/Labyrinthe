d7208a4e08c0b40ff51663db624854f217cdf646
gregoire_delacroix <gregoire_delacroix@ens.univ-artois.fr>
2021-03-10 19:06:32 +0100

    Reréglage fixed

--- src/Plateau.java
+++ src/Plateau.java
@@ -53,13 +53,13 @@ class Plateau {
         Position[] positions = { new Position(2, 2), new Position(2, 4), new Position(4, 2), new Position(4, 4) };
         Objectif[] objectifs = Objectif.values();
         for (int i = 0; i < 4; i++) {
-            couloirs.add(new CouloirFixe(orients[i], Forme.TE, objectifs[i], positions[i]));
+            new CouloirFixe(orients[i], Forme.TE, objectifs[RAND.nextInt(objectifs.length)], positions[i]);
         }
 
         int[][] values = { { 0, 2, 0, 4 }, { 2, 0, 4, 0 }, { 6, 2, 6, 4 }, { 2, 6, 4, 6 } };
         for (int i = 0; i < 4; i++) {
-            couloirs.add(new CouloirFixe(orients[i], Forme.TE, objectifs[i], new Position(values[i][0], values[i][1])));
-            couloirs.add(new CouloirFixe(orients[i], Forme.TE, objectifs[i], new Position(values[i][2], values[i][3])));
+            new CouloirFixe(orients[i], Forme.TE, objectifs[RAND.nextInt(objectifs.length)], new Position(values[i][0],values[i][1]));
+            new CouloirFixe(orients[i], Forme.TE, objectifs[RAND.nextInt(objectifs.length)], new Position(values[i][2],values[i][3]));
         }
     }
 
